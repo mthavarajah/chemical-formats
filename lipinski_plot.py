a6e505243e51c5a8ddad9e24d512813800fa197a
Mathu Thavarajah <mthavarajah10@gmail.com>
2025-06-26 10:43:31 -0400

    lipinski graph

--- lipinski_plot.py
+++ lipinski_plot.py
@@ -27,7 +27,6 @@ def compute_lipinski(chemical_input, input_type):
 def create_lipinski_plot(data, output_path):
     if not data:
         return
-
     trace = go.Scatter(
         x=[d["MW"] for d in data],
         y=[d["logP"] for d in data],
@@ -46,19 +45,14 @@ def create_lipinski_plot(data, output_path):
             font_color="white"
         )
     )
-
     layout = go.Layout(
         title="Lipinski Scatter Plot (Molecular Weight vs LogP)",
         xaxis=dict(title="Molecular Weight"),
         yaxis=dict(title="LogP"),
         plot_bgcolor='#eeeeee',
-
     )
-
     fig = go.Figure(data=[trace], layout=layout)
-
     fig_html = fig.to_html(full_html=False)
-
     with open(output_path, 'w') as f:
         f.write('<div id="lipinski-plot">\n')
         f.write(fig_html)
@@ -66,13 +60,11 @@ def create_lipinski_plot(data, output_path):
 def process_lipinski_inputs(input_list, output_dir="temp_files", filename="lipinski_plot.html"):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-
     lipinski_data = []
     for input_type, chemical_input in input_list:
         props = compute_lipinski(chemical_input, input_type)
         if props:
             lipinski_data.append(props)
-
     output_path = os.path.join(output_dir, filename)
     create_lipinski_plot(lipinski_data, output_path)
     return filename
